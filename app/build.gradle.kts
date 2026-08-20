8610b0bf42bc27a7f90902af0c4d1531bf3b8787
me696979 <me696979@hotmail.com>
2026-08-20 18:10:16 -0400

    Rebrand app version to Scannerz Network 0.2.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "huntington.localscannerapp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.2.5"
+        versionCode = 8
+        versionName = "0.2.6"
     }
 
     signingConfigs {
