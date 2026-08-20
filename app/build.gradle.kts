855b6fa10731d2f8863656a0eb7936c87fd8e7f2
me696979 <me696979@hotmail.com>
2026-08-19 22:06:10 -0400

    Bump scanner app to 0.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "net.aaronznetworking.scanner"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.2.3"
+        versionCode = 6
+        versionName = "0.2.4"
     }
 
     compileOptions {
