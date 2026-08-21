93c0b6d82908bf6d3b2f0bb3ebd9d649228dc092
me696979 <me696979@hotmail.com>
2026-08-20 23:12:43 -0400

    Bump Scannerz Network to version 0.2.8 code 10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "huntington.localscannerapp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.2.7"
+        versionCode = 10
+        versionName = "0.2.8"
     }
 
     signingConfigs {
