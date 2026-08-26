0de40f9fa4220ea6449201c3f474a17787670a8c
me696979 <me696979@hotmail.com>
2026-08-26 16:17:24 -0400

    Add Scannerz Network Admin activity

---
+++ adminapp/src/main/java/net/aaronznetworking/scanneradmin/MainActivity.kt
@@ -0,0 +1,100 @@
+package net.aaronznetworking.scanneradmin
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import android.webkit.CookieManager
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+    private lateinit var progress: ProgressBar
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.adminWebView)
+        progress = findViewById(R.id.pageProgress)
+
+        CookieManager.getInstance().setAcceptCookie(true)
+        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true)
+
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            databaseEnabled = true
+            allowFileAccess = false
+            allowContentAccess = false
+            builtInZoomControls = false
+            displayZoomControls = false
+            mediaPlaybackRequiresUserGesture = true
+            userAgentString = userAgentString + " ScannerzNetworkAdmin/0.1.0"
+        }
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                progress.progress = newProgress
+                progress.visibility = if (newProgress >= 100) View.GONE else View.VISIBLE
+            }
+        }
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                val url = request?.url?.toString().orEmpty()
+                return if (url.startsWith(BASE_URL)) {
+                    false
+                } else {
+                    true
+                }
+            }
+        }
+
+        onBackPressedDispatcher.addCallback(
+            this,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if (webView.canGoBack()) {
+                        webView.goBack()
+                    } else {
+                        finish()
+                    }
+                }
+            }
+        )
+
+        if (savedInstanceState == null) {
+            webView.loadUrl(ADMIN_URL)
+        } else {
+            webView.restoreState(savedInstanceState)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        webView.saveState(outState)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        webView.stopLoading()
+        webView.webChromeClient = null
+        webView.webViewClient = null
+        webView.destroy()
+        super.onDestroy()
+    }
+
+    companion object {
+        private const val BASE_URL = "https://scannerlive.aaronznetworking.net"
+        private const val ADMIN_URL = "$BASE_URL/admin/"
+    }
+}
