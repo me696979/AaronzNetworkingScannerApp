c421b961de73fa1423fa9d79cf7d9ac4cd0586aa
me696979 <me696979@hotmail.com>
2026-08-26 20:49:38 -0400

    Bump user app to v0.2.17 code 19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "huntington.localscannerapp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 18
-        versionName = "0.2.16"
+        versionCode = 19
+        versionName = "0.2.17"
     }
 
     signingConfigs {
