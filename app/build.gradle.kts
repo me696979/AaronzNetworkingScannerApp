b1213460d15146ba650195688072232dc5b6e0ad
me696979 <me696979@hotmail.com>
2026-09-23 18:37:54 -0400

    Bump Android app to 0.2.22

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "huntington.localscannerapp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 23
-        versionName = "0.2.21"
+        versionCode = 24
+        versionName = "0.2.22"
     }
 
     signingConfigs {
